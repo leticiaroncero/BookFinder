9fd13956369ff34ef7e5c242b9859ebf51f98ec4
Leticia Roncero <53988147+leticiaroncero@users.noreply.github.com>
2019-12-26 21:49:23 -0800

    add app data - list of books

---
+++ app/data/friends.js
@@ -0,0 +1,113 @@
+var books = [
+    {
+        "title": "To Kill a Mockingbird",
+        "photo": "https://i.gr-assets.com/images/S/compressed.photo.goodreads.com/books/1553383690l/2657.jpg",
+        "scores": [
+            4,
+            5,
+            4,
+            3,
+            5,
+            5,
+            2,
+            5,
+            3,
+            4,
+        ]
+    }, {
+        "title": "A Game of Thrones",
+        "photo": "https://images-na.ssl-images-amazon.com/images/I/51DA1hQNnJL._SX324_BO1,204,203,200_.jpg",
+        "scores": [
+            4,
+            5,
+            1,
+            1,
+            4,
+            3,
+            2,
+            5,
+            1,
+            4,
+        ]
+    },
+    {
+        "title": "The Hunger Games",
+        "photo": "https://upload.wikimedia.org/wikipedia/en/thumb/3/39/The_Hunger_Games_cover.jpg/220px-The_Hunger_Games_cover.jpg",
+        "scores": [
+            4,
+            1,
+            1,
+            1,
+            1,
+            3,
+            4,
+            4,
+            2,
+            5,
+        ]
+    },
+    {
+        "title": "Harry Potter Series",
+        "photo": "https://images-na.ssl-images-amazon.com/images/I/51HSkTKlauL._SX346_BO1,204,203,200_.jpg",
+        "scores": [
+            3,
+            3,
+            1,
+            4,
+            2,
+            1,
+            5,
+            5,
+            4,
+            5,
+        ]
+    },
+    {
+        "title": "Nineteen Eighty-Four (1984)",
+        "photo": "https://i.gr-assets.com/images/S/compressed.photo.goodreads.com/books/1532714506l/40961427._SX318_.jpg",
+        "scores": [
+            3,
+            1,
+            2,
+            3,
+            1,
+            5,
+            2,
+            5,
+            2,
+            5,
+        ]
+    },
+    {
+        "title": "The Perks of Being a Wallflower",
+        "photo": "https://images-na.ssl-images-amazon.com/images/I/61KNHXmOrvL.jpg",
+        "scores": [
+            3,
+            1,
+            3,
+            5,
+            3,
+            4,
+            2,
+            3,
+            3,
+            2,
+        ]
+    },
+    {
+        "title": "Pippi Longstocking",
+        "photo": "https://images-na.ssl-images-amazon.com/images/I/51AJzl2N0VL._SX349_BO1,204,203,200_.jpg",
+        "scores": [
+            2,
+            1,
+            1,
+            4,
+            2,
+            3,
+            5,
+            3,
+            4,
+            3,
+        ]
+    }
+];
