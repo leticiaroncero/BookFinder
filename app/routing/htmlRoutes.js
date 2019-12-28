2da81cb2105697a75438e2148555f2bcfb310e12
Leticia Roncero <53988147+leticiaroncero@users.noreply.github.com>
2019-12-27 23:38:41 -0800

    initial routing for html

---
+++ app/routing/htmlRoutes.js
@@ -0,0 +1,14 @@
+var express = require("express");
+var path = require("path");
+
+var router = express.Router();
+
+router.get("/survey", function (req, res) {
+    res.sendFile(path.join(__dirname + '/../public/survey.html'));
+});
+
+router.get("*", function (req, res) {
+    res.sendFile(path.join(__dirname + '/../public/home.html'));
+});
+
+module.exports = router;
---
+++ server.js
@@ -0,0 +1,19 @@
+var express = require("express");
+
+var app = express();
+var PORT = 3000;
+
+app.use(express.static("public"));
+
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
+
+var routes = require("./app/routing/htmlRoutes.js");
+app.use(routes);
+
+app.listen(PORT, function () {
+    console.log("App listening on PORT " + PORT);
+});
+
+
+
