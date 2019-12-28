c9ebadfcb17827712ca7fe3969008103325cd934
Leticia Roncero <53988147+leticiaroncero@users.noreply.github.com>
2019-12-28 11:58:19 -0800

    add links to API Books List and Github Repo

--- server.js
+++ server.js
@@ -3,7 +3,7 @@ var express = require("express");
 var app = express();
 var PORT = 3000;
 
-app.use(express.static("public"));
+app.use(express.static("app/public"));
 
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
