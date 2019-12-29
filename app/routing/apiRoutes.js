b0f5ea78fd896ac85ca5194543e618cd2f7eab7f
Leticia Roncero <53988147+leticiaroncero@users.noreply.github.com>
2019-12-29 15:43:04 -0800

    implement best match algorithm

--- app/routing/apiRoutes.js
+++ app/routing/apiRoutes.js
@@ -8,8 +8,27 @@ router.get("/api/books", function (req, res) {
 });
 
 router.post("/api/books", function (req, res) {
+    var userScores = req.body.scores;
+
+    var bestMatchScore;
+    var bestMatch;
+    for (var i = 0; i < books.length; i++) {
+        var currentBookScores = books[i].scores;
+
+        var totalDifference = 0;
+        for (var j = 0; j < userScores.length; j++) {
+            difference = Math.abs(userScores[j] - currentBookScores[j]);
+            totalDifference += difference;
+        }
+        if (totalDifference < bestMatchScore || bestMatchScore === undefined) {
+            bestMatchScore = totalDifference;
+            bestMatch = books[i];
+        }
+    }
+
     books.push(req.body);
-    res.json(books[0]);
+
+    res.json(bestMatch);
 });
 
 module.exports = router;
