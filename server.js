d8385dafb5c726a5f901699356102e312c681d2d
Leticia Roncero <53988147+leticiaroncero@users.noreply.github.com>
2019-12-29 17:16:16 -0800

    add environment variable for port

--- server.js
+++ server.js
@@ -3,7 +3,7 @@ var apiRoutes = require("./app/routing/apiRoutes.js")
 var htmlRoutes = require("./app/routing/htmlRoutes.js");
 
 var app = express();
-var PORT = 3000;
+var PORT = process.env.PORT || 3000;
 
 app.use(express.static("app/public"));
 
